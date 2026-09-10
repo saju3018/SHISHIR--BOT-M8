ce2aaa2ac551e573d24a9aef6b0fdd3540de3dd4
saju3018 <saju30183@gmail.com>
2026-09-10 11:58:15 +0600

    Update mention.js

--- scripts/cmds/mention.js
+++ scripts/cmds/mention.js
@@ -14,8 +14,8 @@ module.exports = {
 
 	onChat: async function ({ api, event }) {
 		const bossUIDs = [
-			"61590481514968",
-			"61592084390757"
+			"61592841571046",
+			"61592841571046"
 		];
 
 		if (!event.mentions || typeof event.mentions !== "object")
@@ -26,6 +26,8 @@ module.exports = {
 		if (mentionedIDs.some(uid => bossUIDs.includes(uid))) {
 			return api.sendMessage(
 				"Boss ekhon busy ache free hoye reply dibe 🫠🌷",
+				"কিরে মাঙ্গের নাতি শিশির বস কে বারবার মেনশন দিস কে কইতাসি কাজে আছে একটু ব্যস্ত কথা কানে যায় না -🙄🐸🌷",
+				"kaner nise akta thapor dibo👋 boss ke bar bar mention des ke re-😡",
 				event.threadID,
 				event.messageID
 			);
